540b1b3af49dbc5a9d4bf87e71d3a4e04f4669ff
Carlos Cabrera <ccabrera@beerealit.com>
2019-03-22 10:08:08 -0300

    form search field

--- src/widgets/components/search.js
+++ src/widgets/components/search.js
@@ -6,14 +6,21 @@ import './search.css';
 //   )
 // }
 
-const Search = () => (
-  <form className="Search">
+const Search = (props) => (
+  <form
+    className="Search"
+    onSubmit={props.handleSubmit}
+  >
     <input
+      ref={props.setRef}
       type="text"
       placeholder="Busca tu video favorito"
       className="Search-input"
+      name="search"
+      onChange={props.handleChange}
+      value={props.value}
     />
   </form>
 )
 
-export default Search;
+export default Search
